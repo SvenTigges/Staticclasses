e7f6421d7c8317c937f97c89ab8d63b27706ce85
SvenTigges <sven.tigges@web.de>
2021-06-04 19:49:16 +0200

    done

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -7,20 +7,20 @@ public class App {
 	public static void main(String[] args) {
 		
 		
-		// RechnerInst rechnerInst = new RechnerInst();
+		RechnerInst rechnerInst = new RechnerInst();
 		
-		// ausgabe("Ergebnis: " + rechnerInst.summe(1, 1));
-		// ausgabe("Ergebnis: " + rechnerInst.differenz(5, 1));
-		// ausgabe("Ergebnis: " + rechnerInst.produkt(2, 3));
-		// ausgabe("Ergebnis: " + rechnerInst.quotient(2, 4));
+		ausgabe("Ergebnis: " + rechnerInst.summe(1, 1));
+		ausgabe("Ergebnis: " + rechnerInst.differenz(5, 1));
+		ausgabe("Ergebnis: " + rechnerInst.produkt(2, 3));
+		ausgabe("Ergebnis: " + rechnerInst.quotient(2, 4));
 		
 		
-		// ausgabe(preStr + RechnerStat.summe(1, 1));
-		// ausgabe(preStr + RechnerStat.differenz(5, 1));
-		// ausgabe(preStr + RechnerStat.produkt(2, 3));
-		// ausgabe(preStr + RechnerStat.quotient(2, 4));
+		ausgabe(preStr + RechnerStat.summe(1, 1));
+		ausgabe(preStr + RechnerStat.differenz(5, 1));
+		ausgabe(preStr + RechnerStat.produkt(2, 3));
+		ausgabe(preStr + RechnerStat.quotient(2, 4));
 		
-		// ausgabe(preStr + Math.PI);
+		ausgabe(preStr + Math.PI);
 		
 	}
 
